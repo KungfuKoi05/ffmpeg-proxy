db94a7c40437d3317e8ed162e8b4e1439b26a51c
KungfuKoi05 <akoispringette@gmail.com>
2026-05-30 00:00:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-server.js
+git clone https://github.com/kungfukoi05/ffmpeg-proxy.git
+cd ffmpeg-proxy
+git checkout -b claude/slack-session-ZjtdV
+# Then copy the server.js content I created
+git add server.js
+git commit -m "Implement ffmpeg proxy server"
+git push -u origin claude/slack-session-ZjtdV
+
